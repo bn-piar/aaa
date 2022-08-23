54a40a87278b806ad714b8ab363d0b45e5b23b4b
bn-piar <105789105+bn-piar@users.noreply.github.com>
2022-08-23 12:19:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {
+  /* the components you used */
+  Typography
+} from "@mui/material";
+
+/**
+ * how you used the components
+ */
+export default function App() {
+  return (
+    <Typography>
+      MUI example. Please put the code to reproduce the issue in src/App.tsx
+    </Typography>
+  );
+}
